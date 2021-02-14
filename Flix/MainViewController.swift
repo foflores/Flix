eb4aa69a8c53264c21a10eaa1f6088b18098de9c
Favian Flores <floresf1997@gmail.com>
2021-02-14 15:50:52 -0500

    setup details page

--- Flix/MainViewController.swift
+++ Flix/MainViewController.swift
@@ -1,49 +1,46 @@
-//
 //  MainViewController.swift
 //  Flix
 //
 //  Created by Favian Flores on 2/7/21.
-//
 
 import UIKit
 import AlamofireImage
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 	
+	// Outlets
 	@IBOutlet weak var tableView: UITableView!
 	
+	// Global
 	var movies = [[String:Any]]()
 
+	// View Ovverides
     override func viewDidLoad() {
         super.viewDidLoad()
 		
 		tableView.dataSource = self
 		tableView.delegate = self
 		
-		let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-		let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-		let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-		let task = session.dataTask(with: request) { (data, response, error) in
-		   // This will run when the network request returns
-		   if let error = error {
-			  print(error.localizedDescription)
-		   } else if let data = data {
-				let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-				
-			self.movies = dataDictionary["results"] as! [[String:Any]]
-			self.tableView.reloadData()
-			
-		   }
-		}
-		task.resume()
+		loadApiData()
     }
 	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		let cell = sender as! UITableViewCell
+		let indexPath = tableView.indexPath(for: cell)!
+		let movie = movies[indexPath.row]
+		let movieDetailsViewController = segue.destination as! MovieDetailsViewController
+		movieDetailsViewController.movie = movie
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
+	
+	// Table View
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return movies.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+		
 		let movie = movies[indexPath.row]
 		let title = movie["title"] as! String
 		let overview = movie["overview"] as! String
@@ -54,18 +51,23 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
 		cell.movieTitle.text = title
 		cell.movieDescription.text = overview
 		cell.movieArt.af.setImage(withURL: posterUrl!)
+		
 		return cell
 	}
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	// Functions
+	func loadApiData() {
+		let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+		let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+		let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+		let task = session.dataTask(with: request) { (data, response, error) in
+			if let error = error { print(error.localizedDescription) }
+			else if let data = data {
+				let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+				self.movies = dataDictionary["results"] as! [[String:Any]]
+				self.tableView.reloadData()
+		   }
+		}
+		task.resume()
+	}
 }
--- Flix/MovieCell.swift
+++ Flix/MovieCell.swift
@@ -1,27 +1,25 @@
-//
 //  MovieCell.swift
 //  Flix
 //
 //  Created by Favian Flores on 2/11/21.
-//
 
 import UIKit
 
 class MovieCell: UITableViewCell {
-
+	
+	// Outlets
 	@IBOutlet weak var movieTitle: UILabel!
 	@IBOutlet weak var movieDescription: UILabel!
 	@IBOutlet weak var movieArt: UIImageView!
 	
+	// Cell Overrides
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
